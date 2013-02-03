7df24b73e11d4d33416f4711afb18473ee814623
dskolovos <dskolovos@gmail.com>
2013-02-03 01:21:41 +0000

    Added scalability tests

---
+++ com.googlecode.pongo.tests/src/com/googlecode/pongo/tests/blog/ScalabilityTests.java
@@ -0,0 +1,28 @@
+package com.googlecode.pongo.tests.blog;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.pongo.tests.blog.model.Comment;
+import com.googlecode.pongo.tests.blog.model.Post;
+
+public class ScalabilityTests extends BlogTests {
+	
+	@Override
+	@Before
+	public void setup() throws Exception {
+		super.setup();
+	}
+	
+	@Test
+	public void testInsert100000Posts() {
+		for (int i=0;i<100000;i++) {
+			Post post = new Post();
+			post.setTitle("Post " + i);
+			post.save(postCollection);
+		}
+	}
+	
+}
